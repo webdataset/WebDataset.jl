529df403b9630c4fab68189a921b30ad21e222a9
Tom <tmbdev+github@gmail.com>
2021-04-19 09:57:41 -0700

    debug

--- src/WebDataset.jl
+++ src/WebDataset.jl
@@ -83,7 +83,7 @@ function tar_stage(inch, outch; decoders=default_decoders, maxcount=1e30)
         #@show stream
         # foreach(tariterator(stream) |> PartitionBy(itemkey) |> Map(make_sample)) do sample
         foreach(tariterator(stream) |> PartitionBy(itemkey) |> Map(make_sample)) do sample
-            if count < 5;
+            if count < 0;
                 msg = (string(sample)*" "^100)[1:100]
                 @show count, msg
             end
