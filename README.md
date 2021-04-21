21104e6d7d35da986b0606ded71120061c4dcf52
Tom <tmbdev+github@gmail.com>
2021-04-20 22:47:53 -0700

    working pretty well now

--- README.md
+++ README.md
@@ -1,6 +1,270 @@
-# WebDataset
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://ThomasBreuel.github.io/WebDataset.jl/stable)
-[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://ThomasBreuel.github.io/WebDataset.jl/dev)
-[![Build Status](https://github.com/ThomasBreuel/WebDataset.jl/workflows/CI/badge.svg)](https://github.com/ThomasBreuel/WebDataset.jl/actions)
-[![Coverage](https://codecov.io/gh/ThomasBreuel/WebDataset.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/ThomasBreuel/WebDataset.jl)
+# Operation
+
+WebDatasets consists of a collection of tar files ("shards"). Each tar file is read sequentially, and multiple shards may be read in parallel.
+
+Within each tar file, files with a common basename but different extensions are grouped together into training samples. The "basename" is defined here as the directory name plus the file name up to the first "." in the file name.
+
+Training samples for learning stereo models might contain a sequence of samples of the form:
+
+```
+098953.left.jpg
+098953.right.jpg
+098953.depth.png16
+194432.left.jpg
+194432.right.jpg
+194432.depth.png16
+```
+
+Training samples are represented as `Dict{String,Any}` instances in the library.
+
+Dataset loading takes place in multiple stages:
+
+```
+tar file reading -> grouping into samples -> decoding of samples -> augmentation of samples -> general mapping -> batching -> collating
+```
+
+The `dataloader` function will set up such a loading pipeline and run it in parallel in multiple threads. The entire process is described by a dataset descriptor:
+
+```
+@with_kw mutable struct DatasetDescriptor
+    # list of shards
+    sources::Array{Union{String,Cmd}} = []
+    # size of the inline shuffle buffer
+    shuffle::Int = 1000
+    # batchsize (0 for no batching)
+    batchsize::Int = 16
+    # decoding functions
+    decoding::Array{Pair{Union{String,Regex,Renamer},Function}} = [""=>x->x]
+    # augmentation functions
+    augmenting::Array{Pair{Union{String,Regex,Renamer},Function}} = [""=>x->x]
+    # a general mapping function running after augmentation
+    mapping::Function = x->x
+    # collation functions running after batching
+    collating::Array{Pair{Union{String,Regex,Renamer},Function}} = [""=>x->x]
+    # channel size for output channel
+    csize::Int = 100
+    # number of concurrent tasks/shards for loading
+    ntasks::Int = 4
+    # more verbose output
+    verbose::Bool = false
+    # even more verbose output
+    debug::Bool = false
+end
+```
+
+Here, `decoding`, `augmenting`, and `collating` are lists of mapping rules that are searched sequentially for a matching rule.  The default decoders are very simple:
+
+
+```
+default_decoders = [
+    ".cls" => clsdecode,
+    Rename(r".(jpg|jpg|png|p?m)$", ".img") => imdecode,
+    ".json" => jsondecode,
+]
+```
+
+This says that every file ending in `.cls` should be decoded with the `clsdecode` function and every file ending in `.json` should be decoded using `jsondecode`. Files matching the regular expression should have their matching portion renamed to `.img` and should be decoded using the `imdecode` function.
+
+Decoder functions should take `UInt8` arrays containing the binary file content and decode into whatever data structure is desired. Augmentation functions take the decoded outputs as inputs and produce new outputs. Collating functions take a list of decoded items (e.g., all files ending in `.img`) and can, for example, stack them together into arrays.
+
+# Simple Example
+
+In this example, we start by defining a simple `DatasetDescriptor`; we're using the default decoders.
+
+
+```julia
+using Pkg; Pkg.add("Flux")
+```
+
+    [32m[1m    Updating[22m[39m registry at `~/.julia/registries/General`
+    [32m[1m    Updating[22m[39m git-repo `https://github.com/JuliaRegistries/General.git`
+    [32m[1m   Resolving[22m[39m package versions...
+    [32m[1m  No Changes[22m[39m to `~/proj/WebDataset.jl/Project.toml`
+    [32m[1m  No Changes[22m[39m to `~/proj/WebDataset.jl/Manifest.toml`
+
+
+
+```julia
+using WebDataset
+using Images
+using Flux: batch
+
+function image_augmentation(image)
+    # generate a fixed size output image for batching here
+    # here, we just return a mock image for demonstration
+    result = zeros(Float32, 3, 256, 256)
+    return result
+end
+
+augmenting = [
+    # call whatever augmentation you like for individual images
+    ".img" => image_augmentation,
+    # this line ensures that any other sample components are carried along unchanged
+    "" => x->x
+]
+
+collating = [
+    # .cls is turned  into an Array{Int}
+    ".cls" => data->Array{Int}(data),
+    # RGB images are batched using Flux.batch
+    ".img" => l->batch(l),
+]
+
+shards = braceexpand("pipe:curl -L -s http://storage.googleapis.com/nvdata-coco/coco-train2014-seg-{000000..000003}.tar")
+shards = braceexpand("/work-2020/shards/imagenet/imagenet-train-{000000..000146}.tar")[1:4]
+
+desc = DatasetDescriptor(
+    sources=shards,
+    shuffle=1000,
+    batchsize=32,
+    decoding=default_decoders,
+    augmenting=augmenting,
+    collating=collating,
+)
+```
+
+
+
+
+    DatasetDescriptor
+      sources: Array{Union{Cmd, String}}((4,))
+      shuffle: Int64 1000
+      batchsize: Int64 32
+      decoding: Array{Pair{Union{Regex, String, WebDataset.Renamer}, Function}}((3,))
+      augmenting: Array{Pair{Union{Regex, String, WebDataset.Renamer}, Function}}((2,))
+      mapping: #23 (function of type WebDataset.var"#23#33")
+      collating: Array{Pair{Union{Regex, String, WebDataset.Renamer}, Function}}((2,))
+      csize: Int64 100
+      ntasks: Int64 4
+      verbose: Bool false
+      debug: Bool false
+
+
+
+
+Here is a non-threaded loader that's useful for debugging.
+
+
+```julia
+if false
+    ch = debugloader(desc, desc.sources[1:2])
+    sample = take!(ch)
+    @show size(sample[".img"])
+end;
+```
+
+This is the multi-threaded loader used for actual training. It loads samples in parallel but delivers them as a simple iterator.
+
+
+```julia
+desc.ntasks = 4
+(count, total) = (0, 0)
+global last_sample
+@time for sample in dataloader(desc)
+    last_sample = sample
+    count += 1
+    total += length(sample["__key__"])
+    if count % 100 == 1; @show count, total; end
+end
+(count, total)
+```
+
+    (count, total) = (1, 32)
+    (count, total) = (101, 3232)
+    (count, total) = (201, 6432)
+    (count, total) = (301, 9632)
+    (count, total) = (401, 12832)
+    (count, total) = (501, 16032)
+    (count, total) = (601, 19232)
+    (count, total) = (701, 22432)
+    (count, total) = (801, 25632)
+    (count, total) = (901, 28832)
+    (count, total) = (1001, 32032)
+     73.420912 seconds (19.44 M allocations: 121.839 GiB, 2.16% gc time, 0.14% compilation time)
+
+
+
+
+
+    (1052, 33607)
+
+
+
+We are getting a nicely batched sample with additional information about sample keys.
+
+
+```julia
+last_sample
+```
+
+
+
+
+    Dict{String, Any} with 3 entries:
+      "__key__" => ["0010656", "0937756", "0729658", "0587807", "0307037", "0965382", "1158354", "0432406", "0…
+      ".img"    => Float32[0.0 0.0 … 0.0 0.0; 0.0 0.0 … 0.0 0.0; 0.0 0.0 … 0.0 0.0]…
+      ".cls"    => [8, 732, 568, 457, 240, 753, 904, 337, 430, 53, 462, 818, 436, 554, 609, 577, 1, 540, 981]
+
+
+
+Four Julia threads give us about 450 decoded images per second. The code scales completely linearly, though Julia multithreading seems to have some overhead when running many threads in parallel.
+
+
+```julia
+33607/73.4
+```
+
+
+
+
+    457.86103542234326
+
+
+
+Exceptions in the multithreaded code are available here.
+
+
+```julia
+WebDataset.num_exceptions, WebDataset.last_exception
+```
+
+
+
+
+    (Base.Threads.Atomic{Int64}(0), nothing)
+
+
+
+# Under the Hood
+
+The `dataloader` function gives you one particular way of multithreaded data loading and augmentation. It is built from a number of simple iterators that you can reuse and recombine in other ways if you like. You can also use distributed computing for distributed loading and augmentation. The core functionality is expressed by this composition of iterators:
+
+```Julia
+# open either a file or a pipe: source
+stream = generic_open(source)
+
+# iterate over samples in the .tar archive
+raw = sampleiterator(stream)
+
+# shuffle samples with a shuffle buffer
+shuffled = sampleshuffle(raw, desc.shuffle)
+
+# decode the samples based on decoding rules
+decoded = sampletransforms(shuffled, desc.decoding)
+
+# augment the samples based on agumentation rules
+augmented = sampletransforms(decoded, desc.augmenting)
+
+# batch the samples to the given batch size
+batched = samplebatching(augmented, desc.batchsize)
+
+# collate individual fields based on collating rules
+collated = sampletransforms(batched, desc.collating)
+```
+
+
+
+```julia
+
+```
--- src/WebDataset.jl
+++ src/WebDataset.jl
@@ -20,7 +20,7 @@ export sampletransforms
 export samplebatching
 export transpose_batch
 export default_decoders
-export map_by_suffix
+export map_by_rules
 export braceexpand
 export allsplitext
 export loadproc
@@ -29,12 +29,19 @@ export debugloader
 export DatasetDescriptor
 export Rename
 export Continue
+export clsdecode
+export imdecode
+export jsondecode
+export generic_open
 
-global dataloader_tids
+global dataloader_tids = nothing
+global last_exception = nothing
+global num_exceptions = Threads.Atomic{Int}(0)
+global max_exceptions = 999999999
 
 # path names
 
-@noinline function allsplitext(s)
+function allsplitext(s)
     if s == ""; return ("", ""); end
     slash = findlast("/", s)
     slash = slash == nothing ? (0,) : slash
@@ -45,9 +52,19 @@ global dataloader_tids
     return s[1:split-1], s[split:length(s)]
 end
 
+# generic I/O
+
+function generic_open(shard; verbose=false)
+    if startswith(shard, "pipe:")
+        shard = Cmd(Base.shell_split(shard[6:length(shard)]))
+    end
+    verbose && @show shard
+    return open(shard)
+end
+
 # brace expansion for filenames
 
-@noinline function braceexpand(s)
+function braceexpand(s::String)
     expansion = r"[{][0-9]+[.][.][0-9]+[}]"
     m = match(expansion, s)
     if m == nothing
@@ -68,9 +85,17 @@ end
     return result
 end
 
+function braceexpand(l::Vector{String})
+    expanded = braceexpand(l[1])
+    if length(l) < 2
+        return expanded
+    end
+
+end
+
 # .tar file reader
 
-@noinline function substr(header, from, size)
+function substr(header, from, size)
     lo = from+1
     hi = from+size
     s = String(header[lo:hi])
@@ -78,7 +103,7 @@ end
     return rstrip(s, ['\0'])
 end
 
-@noinline function decode_header(header)
+function decode_header(header)
     htype = substr(header, 156, 1)
     fname = substr(header, 0, 100)
     prefix = substr(header, 345, 155)
@@ -90,7 +115,7 @@ end
     return (htype, prefix*fname, size, blocks)
 end
 
-@noinline function next_file(stream)
+function next_file(stream)
     header = Array{UInt8}(undef, 512)
     read!(stream, header)
     htype, fname, n, blocks = decode_header(header)
@@ -99,7 +124,7 @@ end
     return (htype, fname, buffer[1:n])
 end
 
-@noinline @resumable function tariterator(stream)
+@resumable function tariterator(stream)
     count = 0
     while(!eof(stream))
         htype, fname, buffer = next_file(stream)
@@ -113,8 +138,7 @@ end
 
 valid_sample(d) = (d != nothing && length(d) > 1)
 
-@noinline @resumable function sampleiterator(shard::String)
-    stream = open(shard)
+@resumable function sampleiterator(stream)
     current_prefix = nothing
     current_sample = nothing
     for (fname, value) in tariterator(stream)
@@ -136,7 +160,7 @@ end
 
 # shuffling
 
-@noinline @resumable function sampleshuffle(source, bufsize=1000)
+@resumable function sampleshuffle(source, bufsize=1000)
     buffer = []
     iter = iterate(source)
     while iter != nothing
@@ -162,14 +186,16 @@ struct EOF
     key
 end
 
-struct Rename
+abstract type Renamer end
+
+struct Rename <: Renamer
     key
-    value
+    new_key
 end
 
-struct Continue
+struct Continue <: Renamer
     key
-    value
+    new_key
 end
 
 
@@ -184,51 +210,92 @@ function kv_apply(f, key, value)
     end
 end
 
-@noinline function map_by_suffix(sample, transformers)
-    result = Dict()
+function maprule(key::String, value, rule::String, f)
+    !endswith(lowercase(key), rule) && return nothing
+    return (key, f(value))
+end
+
+function maprule(key::String, value, rule::Regex, f)
+    match(rule, lowercase(key)) == nothing && return nothing
+    return (key, f(value))
+end
+
+function maprule(key::String, value, rule::Renamer, f)
+    out = maprule(key, value, rule.key, f)
+    out == nothing && return nothing
+    return (rule.new_key, out[2])
+end
+
+function maprule(key::String, value, rule::Type, f)
+    !isa(value, rule) && return nothing
+    return (key, f(value))
+end
+
+
+function map_by_rules(sample, transformers)
+    result = Dict{String,Any}()
     for (key, value) in sample
         if key[1] == '_'
             result[key] = value
             continue
         end
-        for (suffix, f) in transformers
-            if endswith(lowercase(key), suffix)
-                out = kv_apply(f, key, value)
-                if out isa Rename
-                    result[out.key] = out.value
-                    break
-                elseif out isa Continue
-                    result[out.key] = out.value
-                else
-                    result[key] = out
-                    break
-                end
-            end
+        for (rule, f) in transformers
+            output = maprule(key, value, rule, f)
+            output == nothing && continue
+            new_key, new_value = output
+            result[new_key] = new_value
+            !isa(rule, Continue) && break
         end
     end
     return result
 end
 
-@noinline @resumable function sampletransforms(source, transformers)
+@resumable function sampletransforms(source, transformers; handle="ignore")
     for sample in source
-        @yield map_by_suffix(sample, transformers)
+        result = missing
+        try
+            result = map_by_rules(sample, transformers)
+        catch exn
+            @show exn
+            global last_exception
+            global num_exceptions
+            last_exception = exn
+            Threads.atomic_add!(num_exceptions, 1)
+            if num_exceptions[] > max_exceptions
+                error("too many exceptions")
+            end
+            continue
+        end
+        @yield result
     end
 end
 
+function imdecode(data)
+    return ImageMagick.load_(data)
+end
+
+function clsdecode(data; lo=-999999, hi=999999)
+    value = parse(Int64, String(data))
+    if value < lo || value > hi
+        error("cls value out of range")
+    end
+    return value
+end
+
+function jsondecode(data; query="")
+    query == "" || error("unimplemented")
+    return JSON.parse(String(data))
+end
+
 default_decoders = [
-    ".cls" => data->parse(Int64, String(data)),
-    ".jpg" => data->ImageMagick.load_(data),
-    ".jpeg" => data->ImageMagick.load_(data),
-    ".png" => data->ImageMagick.load_(data),
-    ".ppm" => data->ImageMagick.load_(data),
-    ".pgm" => data->ImageMagick.load_(data),
-    ".pbm" => data->ImageMagick.load_(data),
-    ".json" => data->JSON.parse(String(data)),
+    ".cls" => clsdecode,
+    Rename(r".(jpg|jpg|png|p?m)$", ".img") => imdecode,
+    ".json" => jsondecode,
 ]
 
 # batching
 
-@noinline function transpose_batch(sample)
+function transpose_batch(sample)
     keys = Set{String}(key for d in sample for (key, _) in d)
     result = Dict{String,Any}()
     for key in keys
@@ -240,7 +307,7 @@ default_decoders = [
     return result
 end
 
-@noinline @resumable function samplebatching(source, batchsize; minsize=1)
+@resumable function samplebatching(source, batchsize; minsize=1)
     current_batch = []
     for sample in source
         if batchsize == 0
@@ -260,27 +327,49 @@ end
 # multithreaded loader
 
 @with_kw mutable struct DatasetDescriptor
-    sources::Array{String} = []
+    # list of shards
+    sources::Array{Union{String,Cmd}} = []
+    # size of the inline shuffle buffer
     shuffle::Int = 1000
+    # batchsize (0 for no batching)
     batchsize::Int = 16
-    decoding::Array{Pair{String,Function}} = [""=>x->x]
-    augmenting::Array{Pair{String,Function}} = [""=>x->x]
-    collating::Array{Pair{String,Function}} = [""=>x->x]
+    # decoding functions
+    decoding::Array{Pair{Union{String,Regex,Renamer},Function}} = [""=>x->x]
+    # augmentation functions
+    augmenting::Array{Pair{Union{String,Regex,Renamer},Function}} = [""=>x->x]
+    # a general mapping function running after augmentation
+    mapping::Function = x->x
+    # collation functions running after batching
+    collating::Array{Pair{Union{String,Regex,Renamer},Function}} = [""=>x->x]
+    # channel size for output channel
     csize::Int = 100
+    # number of concurrent tasks/shards for loading
     ntasks::Int = 4
+    # more verbose output
     verbose::Bool = false
+    # even more verbose output
     debug::Bool = false
 end
 
-@noinline function loadproc(desc::DatasetDescriptor, inch::Channel, outch::Channel, eof; maxcount=1e30)
+@resumable function ldebug(it)
+    for sample in it
+        @show sample["__key__"], string(sample)[1:100]
+        @yield sample
+    end
+end
+
+
+function loadproc(desc::DatasetDescriptor, inch::Channel, outch::Channel, eof; maxcount=1e30)
     count = 0
     for source in inch
         desc.debug && @show source
-        raw = sampleiterator(source)
+        stream = generic_open(source)
+        raw = sampleiterator(stream)
         shuffled = sampleshuffle(raw, desc.shuffle)
         decoded = sampletransforms(shuffled, desc.decoding)
         augmented = sampletransforms(decoded, desc.augmenting)
-        batched = samplebatching(augmented, desc.batchsize)
+        mapped = (desc.mapping(x) for x in augmented)
+        batched = samplebatching(mapped, desc.batchsize)
         collated = sampletransforms(batched, desc.collating)
         for batch in collated
             desc.debug && @show batch["__key__"]
@@ -289,16 +378,30 @@ end
             count += 1
             if count > maxcount; return; end
         end
+        close(stream)
     end
     put!(outch, eof)
 end
 
-@noinline @resumable function dataloader(desc::DatasetDescriptor, inch::Channel)
+function checkfailed(tids)
+    for t in tids
+        if istaskfailed(t)
+            wait(t)
+        end
+    end
+end
+
+@resumable function dataloader(desc::DatasetDescriptor, inch::Channel)
+    global num_exceptions
     global dataloader_tids
+    Threads.atomic_xchg!(num_exceptions, 0)
     outch = Channel(desc.csize)
-    dataloader_tids = [Threads.@spawn loadproc(desc, inch, outch, EOF(i)) for i in 1:desc.ntasks]
+    tids = [Threads.@spawn loadproc(desc, inch, outch, EOF(i)) for i in 1:desc.ntasks]
+    dataloader_tids = tids
     running = Set(1:desc.ntasks)
     while true
+        checkfailed(tids)
+        tids = [t for t in tids if !istaskdone(t)]
         sample = take!(outch)
         if isa(sample, EOF)
             delete!(running, sample.key)
@@ -310,10 +413,7 @@ end
 end
 
 function dataloader(desc::DatasetDescriptor)
-    fnames::Array{String} = []
-    for d in desc.sources
-        append!(fnames, braceexpand(d))
-    end
+    fnames = desc.sources
     sources = Channel(length(fnames))
     for fname in fnames
         put!(sources, fname)
@@ -322,7 +422,7 @@ function dataloader(desc::DatasetDescriptor)
     return dataloader(desc, sources)
 end
 
-@noinline function debugloader(desc::DatasetDescriptor, sources; maxcount=10)
+function debugloader(desc::DatasetDescriptor, sources; maxcount=10)
     inch = Channel(length(sources))
     for s in sources; put!(inch, s); end
     close(inch)
